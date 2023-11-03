c3fca654800c0bb3bac189ab7a9a92175dc5259d
WiktorKostera <146847952+WiktorKostera@users.noreply.github.com>
2023-11-03 16:38:55 +0100

    Add files via upload

---
+++ Zestaw_4/Class1.cs
@@ -0,0 +1,57 @@
+﻿namespace Zestaw_4
+{
+    internal class Kolo
+    {
+        private double promien;
+        public static int ile = 0;
+
+        public Kolo(double r)
+        {
+            promien = r;
+            ile++;
+        }
+
+        public Kolo()
+        {
+            ile++;
+        }
+        public static double Obwod(double r)
+        {
+            double obw = 2 * Math.PI * r;
+            return obw;
+        }
+
+        public double Obwod()
+        {
+            double obw = 2 * Math.PI * promien;
+            return obw;
+        }
+    }
+    internal class Prostokat
+    {
+        private double bok1, bok2;
+
+        public Prostokat(double a, double b)
+        {
+            bok1 = a;
+            bok2 = b;
+        }
+
+        public Prostokat()
+        {
+        }
+
+
+        public double Obwod(double a, double b)
+        {
+            double obwod = 2 * a + 2 * b;
+            return obwod;
+        }
+
+        public double Obwod()
+        {
+            double obwod = 2 * bok1 + 2 * bok2;
+            return obwod;
+        }
+    }
+}
---
+++ Zestaw_4/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace Zestaw_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TestKola1();
+            //TestProstokata1();
+            //TestKola2();
+            //TestProstokata2();
+            TestKola3();
+        }
+        static void TestKola1()
+        {
+            //wczytanie danych
+            Console.Write("Podaj promien: ");
+            double r = Double.Parse(Console.ReadLine());
+
+            //statyczne wywolanie metody Obwod
+            double obw = Kolo.Obwod(r);
+
+            //wypisanie wyniku
+            Console.WriteLine("Obwod kola o promieniu {0:0.00} wynosi: {1:0.00}", r, obw);
+        }
+        static void TestProstokata1()
+        {
+            Console.Write("Podaj dlugosc a prostokata: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Podaj dlugosc b prostokata: ");
+            double b = double.Parse(Console.ReadLine());
+
+            Prostokat p = new Prostokat();
+
+            double obw = p.Obwod(a, b);
+
+            Console.WriteLine($"Obwod prostokata o bokach {a} i {b} wynosi {obw}");
+        }
+        static void TestKola2()
+        {
+            Console.Write("Podaj promien: ");
+            double r = Double.Parse(Console.ReadLine());
+
+            //tworzenie instancji klasy Kolo
+            Kolo k = new Kolo(r);
+
+            double obw = k.Obwod();
+
+            Console.WriteLine("Obwod kola o promieniu {0:0.00} wynosi: {1:0.00}", r, obw);
+        }
+        static void TestProstokata2()
+        {
+            Console.Write("Podaj dlugosc a prostokata: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Podaj dlugosc b prostokata: ");
+            double b = double.Parse(Console.ReadLine());
+
+            Prostokat p = new Prostokat(a, b);
+
+            double obw = p.Obwod();
+
+            Console.WriteLine($"Obwod prostokata o bokach {a} i {b} wynosi {obw}");
+        }
+        static void TestKola3()
+        {
+            Kolo k1, k2, k3;
+            k1 = new Kolo();
+            k2 = new Kolo(4.5);
+            k3 = new Kolo(2);
+
+            int ile = Kolo.ile;
+
+            Console.WriteLine("Liczba egzemplarzy klasy Kolo wynosi: {0}", ile);
+        }
+    }
+}
