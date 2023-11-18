1a8728139d13089faa8302d658315c50d949fbe7
WiktorKostera <146847952+WiktorKostera@users.noreply.github.com>
2023-11-18 12:28:19 +0100

    Add files via upload

---
+++ Zestaw_7/Class1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zestaw_7
+{
+    internal class Konto
+    {
+        private static long nadawany_numer = 1000;
+
+        string imie;
+        string nazwisko;
+        public long numer;
+        decimal saldo;
+
+        public Konto(string imie, string nazwisko)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.numer = GenerowanieNumeru();
+            this.saldo = 0;
+        }
+        public void Wplac(decimal kwota)
+        {
+            saldo += kwota;
+        }
+        public bool MoznaWyplacic(decimal kwota)
+        {
+            if (kwota <= saldo)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Wyplac(decimal kwota)
+        {
+            if (MoznaWyplacic(kwota))
+            {
+                saldo -= kwota;
+            }
+            else
+            {
+                Console.WriteLine($"Operacja wyplaty {kwota} nie moze byc wykonana");
+            }
+        }
+        public void Przelej(Konto konto, decimal kwota)
+        {
+            if (MoznaWyplacic(kwota))
+            {
+                saldo -= kwota;
+                konto.saldo += kwota;
+            }
+            else
+            {
+                Console.WriteLine($"Przelew w kwocie {kwota} nie moze byc wykonany");
+            }
+        }
+        public void StanKonta()
+        {
+            Console.WriteLine($"Numer konta to: {numer}, włascicielem jest {imie} {nazwisko}, a saldo wynosi {saldo}");
+        }
+        private static long GenerowanieNumeru()
+        {
+            return nadawany_numer++;
+        }
+        public long Numer
+        {
+            get { return numer; }
+        }
+        public void przelej2(Bank bank, long numer, decimal kwota)
+        {
+            Konto wplywajace = bank.ZnajdzKonto(numer);
+
+            if (wplywajace != null)
+            {
+                Wyplac(kwota);
+                wplywajace.Wplac(kwota);
+            }
+        }
+    }
+    internal class Bank
+    {
+        string nazwa;
+        public Konto[] tablicaKont;
+        private static int ilosc = 0;
+
+        public Bank(string nazwa)
+        {
+            this.nazwa = nazwa;
+            this.tablicaKont = new Konto[10];
+        }
+        public Konto ZalozKonto(string imie, string nazwisko)
+        {
+            if(ilosc < 10)
+            {
+                Konto k = new Konto(imie, nazwisko);
+                tablicaKont[ilosc] = k;
+                ilosc++;
+                return k;
+            }
+            else
+            {
+                Console.WriteLine("Tablica kont jest juz pelna!");
+                return null;
+            }
+        }
+        public void ListaKont()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (tablicaKont[i] != null)
+                {
+                    tablicaKont[i].StanKonta();
+                }
+
+            }
+        }
+        public Konto ZnajdzKonto(long numer)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (tablicaKont[i]?.numer == numer)
+                {
+                    return tablicaKont[i];
+                }
+            }
+            Console.WriteLine($"Konto o numerze {numer} nie istnieje!");
+            return null;
+        }
+    }
+    internal class Komorka
+    {
+        public long numerTelefonu;
+        string odebranySMS;
+
+        public Komorka(long numerTelefonu)
+        {
+            this.numerTelefonu = numerTelefonu;
+            this.odebranySMS = "";
+        }
+        public long Numer
+        {
+            get { return numerTelefonu; }
+        }
+        public void OdbierzSMS(string sms)
+        {
+            odebranySMS = sms;
+        }
+        public void CzytajSMS()
+        {
+            Console.WriteLine(odebranySMS);
+        }
+        public void WyslijSMS(Komorka komorka, string sms)
+        {
+            Console.WriteLine($"Wysyłanie sms od {numerTelefonu} do {komorka.numerTelefonu} o treści: {sms}");
+            komorka.OdbierzSMS(sms);
+        }
+        public void WyslijSMS2(OperatorGSM operatorgsm, long numer, string sms)
+        {
+            Komorka docelowa = operatorgsm.Wyszukaj(numer);
+
+            if(docelowa != null)
+            {
+                docelowa.OdbierzSMS(sms);
+            }
+        }
+    }
+    internal class OperatorGSM
+    {
+        public Komorka[] tablicaKomorek;
+        public int ilosc = 0;
+
+        public OperatorGSM()
+        {
+            tablicaKomorek = new Komorka[10];
+        }
+        public void Rejestruj(Komorka komorka)
+        {
+            if (ilosc < 10)
+            {
+                tablicaKomorek[ilosc] = komorka;
+                ilosc++;
+            }
+            else
+            {
+                Console.WriteLine("Tablica komorek jest juz pelna!");
+            }
+        }
+        public Komorka Wyszukaj(long numer)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (tablicaKomorek[i]?.numerTelefonu == numer)
+                {
+                    return tablicaKomorek[i];
+                }
+            }
+            Console.WriteLine($"Abonent o numerze {numer} nie istnieje bądź nie istnieje badz nie jest zarejestrowany w sieci");
+            return null;
+        }
+
+    }
+}
---
+++ Zestaw_7/Program.cs
@@ -0,0 +1,63 @@
+﻿using Zestaw_7;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //ZADANIE 1
+        /*        Konto k1 = new Konto("Paweł", "Nowak");
+                Konto k2 = new Konto("Jan", "Kowalski");
+
+                k1.Wplac(1000);
+                k2.Wplac(500);
+
+                k1.Wyplac(300);
+
+                k1.Przelej(k2, 500);
+
+                k1.Wyplac(400);
+
+                k1.StanKonta();
+                k2.StanKonta();*/
+
+        //ZADANIE 2
+        /*        Bank b1 = new Bank("mBank");
+                b1.ZalozKonto("Paweł", "Nowak");
+                b1.ZalozKonto("Jan", "Kowalski");
+
+                b1.tablicaKont[0].Wplac(1000);
+                b1.tablicaKont[1].Wplac(500);
+
+                b1.tablicaKont[0].przelej2(b1, b1.tablicaKont[1].numer, 500);
+
+                b1.ListaKont();*/
+
+        //ZADANIE 3
+
+/*        Komorka k1 = new Komorka(567456987);
+        Komorka k2 = new Komorka(444555666);
+
+        k1.WyslijSMS(k2, "Co tam u Ciebie?");
+
+        k2.CzytajSMS();
+
+        k1.WyslijSMS(k2, "Co robisz w weekend?");
+        k1.WyslijSMS(k2, "Piwo w sobote o 18?");
+
+        k2.CzytajSMS();*/
+
+        //ZADANIE 4
+
+        OperatorGSM o1 = new OperatorGSM();
+        Komorka k1 = new Komorka(573465987);
+        Komorka k2 = new Komorka(657435634);
+        o1.Rejestruj(k1);
+        o1.Rejestruj(k2);
+
+        o1.tablicaKomorek[0].WyslijSMS2(o1, o1.tablicaKomorek[1].numerTelefonu, "Co dzisiaj robisz?");
+
+        o1.tablicaKomorek[1].CzytajSMS();
+
+        o1.tablicaKomorek[0].WyslijSMS2(o1, 123123123, "Co to za numer?");
+    }
+}
