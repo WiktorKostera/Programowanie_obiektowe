b9a9ce9213873be33dd361266d1a783c8772f35f
WiktorKostera <146847952+WiktorKostera@users.noreply.github.com>
2023-11-21 16:10:43 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Zestaw_7;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Konto k1 = new Konto("Wiktor", "Kostera");
+        
+        Console.WriteLine(k1.Imie);
+        k1.Imie = "Karol";
+        Console.WriteLine(k1.Imie);
+
+        Console.WriteLine(k1.Nazwisko);
+        k1.Nazwisko = "Kantor";
+        Console.WriteLine(k1.Nazwisko);
+
+        Console.WriteLine(k1.Numer);
+        k1.Numer = 2000;
+        Console.WriteLine(k1.Numer);
+    }
+}
